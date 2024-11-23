05fef30df7d479d7975637cff0a93c3945ed2286
Dmytro Chobotar <dchobotar.fl@gmail.com>
2024-11-22 21:06:22 -0500

    added some comments

--- 03-js-codebase/03-class/04-arrays/script.js
+++ 03-js-codebase/03-class/04-arrays/script.js
@@ -12,64 +12,65 @@ console.log(numbers);
 const arr = new Array('apple', 'banana', 'grape');
 console.log(arr);
 
+// access elements 
 console.log(numbers[3]);
 console.log(numbers[0] + numbers[4]);
 
+// use element in templates and concat
 let fact = `I love ${arr[2]}`;
-console.log(fact);
 
+// length is a property noty a method
 let result = arr.length;
-console.log(result);
 
+// reassign the value of an element
 arr[1] = 'pineapple';
-console.log(arr);
 
 const newNumbers = [10, 20, 30, 40, 50];
 
-console.log(newNumbers);
+// decrease the length of an array
+newNumbers.length = 3;
 
-// newNumbers.length = 3;
-// console.log(newNumbers);
-
+// add a value to the end of the array
 newNumbers[newNumbers.length] = 60;
-console.log(newNumbers);
 
 // Array methods
 const nums = [22, 43, 11, 54, 23, 77];
 
-// add element at the end of the array
+// push() - add element at the end of the array
 nums.push(99);
 
-// remove the last element of the array
+// pop() - remove the last element of the array
 nums.pop();
 
-// add element to the beginning of the array
+// unshift() - add element to the beginning of the array
 nums.unshift(100);
 
-// how do we remove the element from the start of the array
+// shift() - how do we remove the element from the start of the array
 nums.shift();
 
-// reverse an array
+// reverse() - reverse an array
 nums.reverse();
 
-// check if an element exists 
+// includes() - check if an element exists 
 let res = nums.includes(99); // boolean
 
-// get the index of an item
+// indexOf() - get the index of an item
 let index = nums.indexOf(111);
 
-// slice - getting you back a part of array without mutating it
+// slice() - getting you back a part of array without mutating it
 let partOfArray = nums.slice(2);
 partOfArray = nums.slice(1, 5);
 
-// splice - returns a part of the array and removes the items from array permanently
+// splice() - returns a part of the array and removes the items from array permanently
 // partOfArray = nums.splice(1, 4);
-nums.splice(3, 1);
-nums.splice(1, 2);
+// console.log(partOfArray);
 
+// in splice the very first argument is the index where you start 
+// the second argument is how many elements you will return and remove from original array
+nums.splice(3, 1); 
+nums.splice(1, 2);
 console.log(nums);
 
-// console.log(partOfArray);
 
 
 
