ae4a0000268b4d03f33fbf6f3da8530eca934832
Dmytro Chobotar <dchobotar.fl@gmail.com>
2024-12-07 13:46:37 -0500

    Demo for domain generator project

--- 03-js-codebase/07-class/04-array-high-order-functions/script.js
+++ 03-js-codebase/07-class/04-array-high-order-functions/script.js
@@ -5,15 +5,15 @@ console.log("Array High Order Functions");
 // High Order Function - is a function that recieves a function as an arguments
 // The function that is passed as an argument is called callback function.
 
-const technologies = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Bootstrap",
-  "Python",
-  "Flask",
-];
+// const technologies = [
+//   "HTML",
+//   "CSS",
+//   "JavaScript",
+//   "React",
+//   "Bootstrap",
+//   "Python",
+//   "Flask",
+// ];
 
 // // Option 1 - Anonymous function
 // technologies.forEach(function(item){
@@ -63,5 +63,42 @@ const technologies = [
 // const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
 // console.log(evenNumbers);
+
+
+// map() - will allow you to modify the array elements using another array as base
+// this function will return a new function with modified elements 
+const numbers = [10, 20, 30, 40, 50]; // ['Number 10', 'Number 20']
+
+// option 1
+// const newNumbers = [];
+
+// numbers.forEach((num) => {
+//   newNumbers.push(num * 2);
+// });
+
+// option 2
+// const newNumbers = numbers.map((num) => 'Number ' + num);
+
+// console.log(newNumbers);
+
+
+// // reduce() - will allow you to accumulate all the data in a single value
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // let sum = 0; //10
+
+// // arr.forEach((number) => { // 4
+// //   sum = sum + number; // sum = 6 + 4 = 10
+// // });
+
+// // console.log(sum);
+
+// // accumulator/prevoiusValue, currentValue
+// const sum = arr.reduce((acc, cur) => acc + cur, 0);
+
+// console.log(sum, typeof sum);
+
+
+
 
 
--- 03-js-codebase/07-class/05-playground-high-order-functions/script.js
+++ 03-js-codebase/07-class/05-playground-high-order-functions/script.js
@@ -1,144 +1,142 @@
 console.log("Exercise High Order Functions");
 
 const songs = [
-    {
-      name: "Lose Control",
-      singer: "Teddy Swims",
-      genre: "Soul/Pop",
-      yearOfRelease: 2022,
-      duration: 201,
-      copiesSold: 500000,
-      status: {
-        favorite: true,
-        skipped: false,
-        markedForDeletion: false
-      }
+  {
+    name: "Lose Control",
+    singer: "Teddy Swims",
+    genre: "Soul/Pop",
+    yearOfRelease: 2024,
+    duration: 201,
+    copiesSold: 500000,
+    status: {
+      favorite: true,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Paint the Town Red",
-      singer: "Doja Cat",
-      genre: "Hip-Hop/Rap",
-      yearOfRelease: 2023,
-      duration: 217,
-      copiesSold: 800000,
-      status: {
-        favorite: true,
-        skipped: false,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Paint the Town Red",
+    singer: "Doja Cat",
+    genre: "Hip-Hop/Rap",
+    yearOfRelease: 2023,
+    duration: 217,
+    copiesSold: 800000,
+    status: {
+      favorite: true,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Blinding Lights",
-      singer: "The Weeknd",
-      genre: "Synth-Pop",
-      yearOfRelease: 2020,
-      duration: 200,
-      copiesSold: 15000000,
-      status: {
-        favorite: true,
-        skipped: false,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Blinding Lights",
+    singer: "The Weeknd",
+    genre: "Synth-Pop",
+    yearOfRelease: 2020,
+    duration: 200,
+    copiesSold: 15000000,
+    status: {
+      favorite: true,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Someone You Loved",
-      singer: "Lewis Capaldi",
-      genre: "Pop",
-      yearOfRelease: 2019,
-      duration: 182,
-      copiesSold: 10000000,
-      status: {
-        favorite: false,
-        skipped: true,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Someone You Loved",
+    singer: "Lewis Capaldi",
+    genre: "Pop",
+    yearOfRelease: 2019,
+    duration: 182,
+    copiesSold: 10000000,
+    status: {
+      favorite: false,
+      skipped: true,
+      markedForDeletion: false,
     },
-    {
-      name: "Shape of You",
-      singer: "Ed Sheeran",
-      genre: "Pop",
-      yearOfRelease: 2017,
-      duration: 233,
-      copiesSold: 26000000,
-      status: {
-        favorite: true,
-        skipped: false,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Shape of You",
+    singer: "Ed Sheeran",
+    genre: "Pop",
+    yearOfRelease: 2017,
+    duration: 233,
+    copiesSold: 26000000,
+    status: {
+      favorite: true,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Uptown Funk",
-      singer: "Mark Ronson ft. Bruno Mars",
-      genre: "Funk/Pop",
-      yearOfRelease: 2014,
-      duration: 269,
-      copiesSold: 20000000,
-      status: {
-        favorite: false,
-        skipped: false,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Uptown Funk",
+    singer: "Mark Ronson ft. Bruno Mars",
+    genre: "Funk/Pop",
+    yearOfRelease: 2014,
+    duration: 269,
+    copiesSold: 20000000,
+    status: {
+      favorite: false,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Take Me to Church",
-      singer: "Hozier",
-      genre: "Indie Rock",
-      yearOfRelease: 2013,
-      duration: 241,
-      copiesSold: 15000000,
-      status: {
-        favorite: true,
-        skipped: false,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Take Me to Church",
+    singer: "Hozier",
+    genre: "Indie Rock",
+    yearOfRelease: 2013,
+    duration: 241,
+    copiesSold: 15000000,
+    status: {
+      favorite: true,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Billie Jean",
-      singer: "Michael Jackson",
-      genre: "Pop",
-      yearOfRelease: 1983,
-      duration: 293,
-      copiesSold: 100000000,
-      status: {
-        favorite: true,
-        skipped: false,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Billie Jean",
+    singer: "Michael Jackson",
+    genre: "Pop",
+    yearOfRelease: 1983,
+    duration: 293,
+    copiesSold: 100000000,
+    status: {
+      favorite: true,
+      skipped: false,
+      markedForDeletion: false,
     },
-    {
-      name: "Bohemian Rhapsody",
-      singer: "Queen",
-      genre: "Rock",
-      yearOfRelease: 1975,
-      duration: 354,
-      copiesSold: 6000000,
-      status: {
-        favorite: false,
-        skipped: true,
-        markedForDeletion: false
-      }
+  },
+  {
+    name: "Bohemian Rhapsody",
+    singer: "Queen",
+    genre: "Rock",
+    yearOfRelease: 1975,
+    duration: 354,
+    copiesSold: 6000000,
+    status: {
+      favorite: false,
+      skipped: true,
+      markedForDeletion: false,
     },
-    {
-      name: "Drivers License",
-      singer: "Olivia Rodrigo",
-      genre: "Pop",
-      yearOfRelease: 2021,
-      duration: 242,
-      copiesSold: 12000000,
-      status: {
-        favorite: false,
-        skipped: true,
-        markedForDeletion: false
-      }
-    }
-  ];
+  },
+  {
+    name: "Drivers License",
+    singer: "Olivia Rodrigo",
+    genre: "Pop",
+    yearOfRelease: 2021,
+    duration: 242,
+    copiesSold: 12000000,
+    status: {
+      favorite: false,
+      skipped: true,
+      markedForDeletion: false,
+    },
+  },
+];
 
 // console.log(songs[0]);
 
 // Display only the names, singers and year of songs
 // songs.forEach((song, index) => console.log(`${index + 1} - ${song.name} by ${song.singer}: ${song.yearOfRelease}`));
-
-
 
 // const song = {
 //     name: "Lose Control",
@@ -154,29 +152,21 @@ const songs = [
 //     }
 //   }
 
-//   // === 
+//   // ===
 // console.log(song.genre.includes('Rock'));
-
-
-
-
-
 
 // Get only Rock genre songs
 
-const rockSongs = songs.filter(function(song){
-    return song.genre.includes('Rock'); // True
-});
+// const rockSongs = songs.filter(function(song){
+//     return song.genre.includes('Rock'); // True
+// });
 
-const rockSongs2 = songs.filter((song) => song.genre.includes('Rock'));
+// const rockSongs2 = songs.filter((song) => song.genre.includes('Rock'));
 
 // console.log(rockSongs);
 
-
 // console.log('Rock Songs:');
 // rockSongs.forEach(song => console.log(`   - ${song.name}`));
-
-
 
 // // Get only pop songs
 // const popSongs = songs.filter((song) => song.genre === 'Pop');
@@ -206,10 +196,74 @@ const rockSongs2 = songs.filter((song) => song.genre.includes('Rock'));
 
 // console.log(songs);
 
-
 // Product list, each product will represent few properties
 
+// {
+//   name: "Lose Control",
+//   singer: "Teddy Swims",
+//   genre: "Soul/Pop",
+//   yearOfRelease: 2022,
+//   duration: 201,
+//   copiesSold: 500000,
+//   status: {
+//     favorite: true,
+//     skipped: false,
+//     markedForDeletion: false
+//   }
+// }
 
+// =========
 
+// {
+//   name: "Lose Control",
+//   singer: "Teddy Swims"
+// }
 
+// // Transformation
+// console.log(songs);
+// const songNames = songs.map((song) => {return song.name});
+// console.log(songNames);
 
+// // chaining functions
+// const shortSongs = songs.map((song) => {
+//   return {$
+//     name: song.name,
+//     singer: song.singer,
+//     isNew: song.yearOfRelease >= 2023
+//   }
+// })
+// .filter((song) => song.isNew === true);
+
+// const cart = [
+//   {id: 101, name: 'Laptop', price: 1299.99},
+//   {id: 102, name: 'Phone', price: 999},
+//   {id: 103, name: 'Keyboard', price: 99.50},
+// ];
+
+// const total = cart.reduce((acc, cur) => acc + cur.price, 0);
+// console.log('Total: ', total);
+
+// All permutations
+
+const whose = ["mine", "yours", "ours"];
+const things = ["cap", "shirt", "pants", "coat"];
+const colors = ["black", "blue", "white"];
+
+// ['cap black', 'cap blue', 'cap white', 'shirt black', 'shirt blue', 'cashirtp white']
+const combinations = [];
+
+for (const who of whose) {
+  for (const thing of things) {
+    for (const color of colors) {
+      combinations.push(`${who} ${thing} ${color}`)
+    }
+  }
+}
+
+whoes.forEach((who) => {
+  things.forEach((thing) => {
+    colors.forEach((color) => {
+      console.log(`${who} ${thing} ${color}`);
+    });
+  });
+});
